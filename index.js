53f2efde2168852bb2d3156615d1da71e7bc9bba
CodeSandbox <support@codesandbox.io>
2023-11-29 19:57:17 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log("Hello CodeSandbox");
+//Objects
+const vehicle = {
+  color: "blue",
+  hp: 4000,
+  year: 1989,
+  "active registration": true,
+};
+
+// console.log(vehicle.hp); // 4000
+// console.log(vehicle["color"]); //blue
+// console.log(vehicle["active registration"]); // true
+
+// Changing Object Properties and Values
+vehicle.color = "red"; // changing a existing property
+vehicle.make = "Audi"; // adding a new key:value pair
+delete vehicle["active registration"]; // removing an existing property
+vehicle.doors = 4;
+
+for (let key in vehicle) {
+  console.log(key + ": " + vehicle[key]);
+  //   console.log(vehicle.key);
+}
+
+// Other Objects
+const car = {
+  color: "blue",
+  year: 1989,
+  make: "Audi",
+  engine: {
+    cylinders: 6,
+    hp: 4000,
+    size: 3.2,
+  },
+  passengers: [
+    {
+      name: "chris",
+      isCool: true,
+    },
+    {
+      name: "Younghyun",
+      isCool: true,
+    },
+  ],
+};
+//
+// const car2 = car;
+// delete car2.color;
+// console.log(car);
+
+// console.log(car === car2);
+
+// console.log(car.passengers[1].name);
+
+// console.log(car.engine.size);
